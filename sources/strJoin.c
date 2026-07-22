44844d99c8e6d62a81c4938c64d5c85f1c6b4e64
JulienPR <julien.pr86@gmail.com>
2026-07-22 15:29:48 +0200

    feat : strJoin is now implemented

--- sources/strJoin.c
+++ sources/strJoin.c
@@ -2,7 +2,19 @@
 
 char	*strJoin(const char **s, const char *sep)
 {
-	if (NULL == s || NULL == sep)
+	size_t	index;
+	char	*join;
+
+	if (NULL == s || NULL == *s || NULL == sep)
 		return (NULL);
-	return (NULL);
+	join = NULL;
+	index = 0;
+	while (*(s + index))
+	{
+		strAppend(&join, *(s + index));
+		if (*(s + index + 1))
+			strAppend(&join, sep);
+		++index;
+	}
+	return (join);
 }
